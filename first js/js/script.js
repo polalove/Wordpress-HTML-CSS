3156237d2e0d761c60db7ff6363e57bf32077bdf
Ivan <example@gmail.com>
2022-12-09 22:06:23 +0300

    first application

--- first js/js/script.js
+++ first js/js/script.js
@@ -1,3 +1,23 @@
-let isTrue = false, 
-    isFalse = true;
-console.log(isTrue || !isFalse);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms); 
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+
+    },
+    actors: {},
+    genres: [],
+    privat: false    
+};
+
+let a = prompt('Один из последних просмотренных фильмов', '');
+let b = prompt('На сколько оцените его?', '');
+let c = prompt('Один из последних просмотренных фильмов', '');
+let d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
